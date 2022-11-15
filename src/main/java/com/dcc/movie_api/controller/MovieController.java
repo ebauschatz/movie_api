4db5a397c98042b933bad1608002764c3bdb0213
ebauschatz <eaenglish113@gmail.com>
2022-11-15 13:44:17 -0600

    new post and get endpoints

--- src/main/java/com/dcc/movie_api/controller/MovieController.java
+++ src/main/java/com/dcc/movie_api/controller/MovieController.java
@@ -3,9 +3,9 @@ package com.dcc.movie_api.controller;
 import com.dcc.movie_api.data.Movie;
 import com.dcc.movie_api.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
@@ -15,5 +15,20 @@ public class MovieController {
     @PostMapping("/addMovie")
     public Movie addMovie(@RequestBody Movie movie) {
         return movieService.saveMovie(movie);
+    }
+
+    @GetMapping("/movies")
+    public List<Movie> getAllMovies() {
+        return movieService.getAllMovies();
+    }
+
+    @GetMapping("/movies/{id}")
+    public Movie getMovieById(@PathVariable Integer id) {
+        return movieService.getMovieById(id);
+    }
+
+    @GetMapping("/findByGenre/{genre}")
+    public List<Movie> getMovieByGenre(@PathVariable String genre) {
+        return movieService.getByGenre(genre);
     }
 }
--- src/main/java/com/dcc/movie_api/service/MovieService.java
+++ src/main/java/com/dcc/movie_api/service/MovieService.java
@@ -5,6 +5,8 @@ import com.dcc.movie_api.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MovieService {
     @Autowired
@@ -12,5 +14,17 @@ public class MovieService {
 
     public Movie saveMovie(Movie movie) {
         return movieRepository.save(movie);
+    }
+
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Movie getMovieById(Integer id) {
+        return movieRepository.findById(id).orElse(null);
+    }
+
+    public List<Movie> getByGenre(String genre) {
+        return movieRepository.findByGenre(genre);
     }
 }
